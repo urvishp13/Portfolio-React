1a8b755c9e7de7757f958d5b86847fa912ac1776
Picasso <urvishp13@gmail.com>
2024-02-01 12:05:51 -0500

    did footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+
 export default function Footer() {
     return (
         <footer>
-            
+            <div className="copyright">
+                © 2023 Urvish Patel 
+            </div>
+            <div className="socials">
+                <a href="https://www.linkedin.com/in/urvishpatel13/" target="_blank"><FaLinkedin /></a>
+                <a href="https://github.com/urvishp13" target="_blank"><FaGithubSquare /></a>
+            </div>
         </footer>
     )
 }
