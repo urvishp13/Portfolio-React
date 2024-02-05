eb2c4435a754bbbd89c46dbb75457f01466060e4
Picasso <urvishp13@gmail.com>
2024-02-05 14:22:08 -0500

    made feedback edits--mobile view

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,12 +1,22 @@
-import { FaLink, FaGithub } from "react-icons/fa";
+import { FaLink, FaGithub } from "react-icons/fa"
+import { useState, useEffect } from "react"
 
 export default function Project(props) {
+
+    const [screenshot, setScreenshot] = useState(window.screen.width > 500 ? props.imgDesktop : props.imgMobile)
+
+    useEffect(function() {
+        window.addEventListener("resize", function() {
+            setScreenshot(window.screen.width > 500 ? props.imgDesktop : props.imgMobile)
+        })
+    }, [window.screen.width])
+
     return (
         <div className="project">
-            <img src={props.img} alt={props.alt}/>
+            <img src={screenshot} alt={props.alt}/>
             <p>{props.desc}</p>
             <a href={props.live} target="_blank">Live <FaLink /></a>
-            <a href={props.github} target="_blank">GitHub <FaGithub /></a>
+            <a href={props.github} target="_blank">Code <FaGithub /></a>
         </div>
     )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -21,43 +21,49 @@ export default function Work() {
             <h2>Projects</h2>
             <div className="projects">
                 <Project 
-                    img="screenshots/nba-finals-revisited.png"
-                    alt="mobile view screenshot of NBA Finals Revisited project"
+                    imgDesktop="screenshots/desktop/nba-finals-revisited.png"
+                    imgMobile="screenshots/mobile/nba-finals-revisited.png"
+                    alt="screenshot of NBA Finals Revisited project"
                     desc="An interactive basketball game; go up against an opponent and see if you can win."
                     live="https://2008-nba-finals-revisited.netlify.app/"
                     github="https://github.com/urvishp13/2008-NBA-Finals"
                 />
                 <Project 
-                    img="screenshots/we-are-the-champions.png"
-                    alt="mobile view screenshot of We Are The Champions project"
+                    imgDesktop="screenshots/desktop/we-are-the-champions.png"
+                    imgMobile="screenshots/mobile/we-are-the-champions.png"
+                    alt="screenshot of We Are The Champions project"
                     desc="Write a compliment to your friend. Post, delete, and like what you see."
                     live="https://one-community.netlify.app/"
                     github="https://github.com/urvishp13/We-Are-The-Champions"
                 />
                 <Project 
-                    img="screenshots/password-generator.png"
-                    alt="mobile view screenshot of Password Generator project"
+                    imgDesktop="screenshots/desktop/password-generator.png"
+                    imgMobile="screenshots/mobile/password-generator.png"
+                    alt="screenshot of Password Generator project"
                     desc="Need an app that suggests strong passwords for you? Well, here you go."
                     live="https://premium-password.netlify.app/"
                     github="https://github.com/urvishp13/Password-Generator"
                 />
                 <Project 
-                    img="screenshots/best-bank.png"
-                    alt="mobile view screenshot of Best Bank project"
+                    imgDesktop="screenshots/desktop/best-bank.png"
+                    imgMobile="screenshots/mobile/best-bank.png"
+                    alt="screenshot of Best Bank project"
                     desc="A prototype for a banking app with a sleek design."
                     live="https://bestbank.netlify.app/"
                     github="https://github.com/urvishp13/BestBank"
                 />
                 <Project 
-                    img="screenshots/color-generator.png"
-                    alt="mobile view screenshot of Color Palette Generator project"
+                    imgDesktop="screenshots/desktop/color-generator.png"
+                    imgMobile="screenshots/mobile/color-generator.png"
+                    alt="screenshot of Color Palette Generator project"
                     desc="Do you need help finding a color palette for your next project? Well look no further because the Color Me Silly app will do all the searching for you."
                     live="https://color-me-silly.netlify.app/"
                     github="https://github.com/urvishp13/Color-Scheme-Generator"
                 />
                 <Project 
-                    img="screenshots/learning-journal.png"
-                    alt="mobile view screenshot of Learning Journal project"
+                    imgDesktop="screenshots/desktop/learning-journal.png"
+                    imgMobile="screenshots/mobile/learning-journal.png"
+                    alt="screenshot of Learning Journal project"
                     desc="A multi-page learning journal."
                     live="https://pathway-to-education.netlify.app/"
                     github="https://github.com/urvishp13/Learning-Journal"
