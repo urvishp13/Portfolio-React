98cb6ddceba69d874ddc494780cf0d5e4272bdf0
Picasso <urvishp13@gmail.com>
2024-02-07 22:46:29 -0500

    made portfolio desktop-view first responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,26 +3,28 @@ import questionMark from "../images/question-mark.png";
 export default function About() {
   return (
     <section id="about-section" className="about-section">
-      <div
-        className="question"
-        style={{ backgroundImage: `url(${questionMark})` }}
-      >
-        <p>WHO IS HE</p>
-      </div>
-      <div className="answer">
-        <p>
-          Urvish is a versatile individual. He is a <strong>Mechanical Engineering</strong> grad,
-          a <strong>Music Producer</strong>, and a <strong>Frontend Developer</strong>. How are all these
-          experiences interrelated? They all utilize creativity and design.
-        </p>
-        <p>
-          Music has provided Urvish with the ability to utilize objective and
-          subjective creativity—a skill necessary in UI design. Engineering has
-          allowed Urvish the ability to make practicial judgments on proper UX
-          design. And Frontend Development has provided Urvish with the outlet
-          to utilize all his past experiences all while doing what he loves:
-          making his creations tangible.
-        </p>
+      <div className="container">
+        <div
+          className="question"
+          style={{ backgroundImage: `url(${questionMark})` }}
+        >
+          <p>WHO IS HE</p>
+        </div>
+        <div className="answer">
+          <p>
+            Urvish is a versatile individual. He is a <strong>Mechanical Engineering</strong> grad,
+            a <strong>Music Producer</strong>, and a <strong>Frontend Developer</strong>. How are all these
+            experiences interrelated? They all utilize creativity and design.
+          </p>
+          <p>
+            Music has provided Urvish with the ability to utilize objective and
+            subjective creativity—a skill necessary in UI design. Engineering has
+            allowed Urvish the ability to make practicial judgments on proper UX
+            design. And Frontend Development has provided Urvish with the outlet
+            to utilize all his past experiences all while doing what he loves:
+            making his creations tangible.
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 export default function Home() {
     return (
         <section id="home-section" className="home-section">
-            <img className="home-section-img" src="images/urvish-square-2.png" alt="picture of Urvish" />
-            <div className="professional-details">
-                <p>.introducing<span>(Urvish Patel)</span></p>
-                <p>.title<span>(Frontend Developer)</span></p>
-                <p>.education<span>(Scrimba Frontend Developer Bootcamp)</span></p>
-                <p>.experience<span>(Software Developer)</span></p>
+            <div className="container">
+                <img className="home-section-img" src="images/urvish-square-2.png" alt="picture of Urvish" />
+                <div className="professional-details">
+                    <p>.introducing<span>(Urvish Patel)</span></p>
+                    <p>.title<span>(Frontend Developer)</span></p>
+                    <p>.education<span>(Scrimba Frontend Developer Bootcamp)</span></p>
+                    <p>.experience<span>(Software Developer)</span></p>
+                </div>
             </div>
         </section>
     )
